a687cfcbbbc4aff8f1acc6b0e9e844f5f4b428ce
NdegwaJulius <juliusndegwa2@gmail.com>
2023-01-27 10:40:46 +0300

    initializing signout with firebase

--- lib/Jobs/jobs_screen.dart
+++ lib/Jobs/jobs_screen.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:kazi_board/States/user_state.dart';
 
 class JobScreen extends StatefulWidget {
   @override
@@ -6,11 +8,21 @@ class JobScreen extends StatefulWidget {
 }
 
 class _JobScreenState extends State<JobScreen> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Kazi Board'),
+      ),
+      body: ElevatedButton(
+        onPressed: () {
+          _auth.signOut();
+          Navigator.canPop(context) ? Navigator.pop(context) : null;
+          Navigator.pushReplacement(
+              context, MaterialPageRoute(builder: (_) => UserState()));
+        },
+        child: const Text('Logout'),
       ),
     );
   }
